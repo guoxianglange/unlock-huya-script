b2eaed7d2deb3f28e1f51abee3f9dc8f49564731
lange <45199004+guoxianglange@users.noreply.github.com>
2025-02-03 13:38:58 +0800

    Update unlock-huya.user.js
    修复切换线路后需要刷新页面才能解锁扫码画质的问题。
    
    修复切换线路后自动降画质的问题（还是会有文字提示，但画质不会降）。

--- unlock-huya.user.js
+++ unlock-huya.user.js
@@ -11,18 +11,37 @@
 (function () {
     'use strict';
 
+    // 记录上次 alert 时间
+    let lastAlertTime = 0;
+    // 设置 1 秒冷却时间
+    const alertCooldown = 1000;
+
     // 修改属性值
     const checkElement = setInterval(() => {
         try {
-            const targetElement = document.querySelector('.player-videotype-list li');
+            const targetElement = document.querySelectorAll('.player-videotype-list li');
             if (targetElement) {
                 // 修改属性值
-                if ($(targetElement).data("data").status !== 9) {
-                    $(targetElement).data("data").status = 9;
-                    alert("成功解锁扫码限制！");
-                    // 清除定时器
-                    //clearInterval(checkElement);
-                }
+                targetElement.forEach((element) => {
+                    try {
+                        var isFlag = false
+                        if ($(element).data("data").status !== 9) {
+                            $(element).data("data").status = 9;
+                            isFlag = true
+                        }
+                        if (isFlag) {
+                            const now = Date.now();
+                            if (now - lastAlertTime > alertCooldown) {
+                                alert("成功解锁扫码限制！");
+                                lastAlertTime = now; // 更新上次 alert 时间
+                            }
+                        }
+                    } catch (e) {
+                        // 跳过错误信息
+                    }
+                })
+                // 清除定时器
+                //clearInterval(checkElement);
             }
         } catch (error) {
             // 跳过错误信息
